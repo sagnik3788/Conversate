feccc8bb9fa525fbb299d28afcf1605935a6f016
sagnik3788 <116512372+sagnik3788@users.noreply.github.com>
2023-07-13 00:04:18 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -1,42 +1,34 @@
-const express= require('express')
-const http =require('http')
+const path = require('path')
+const http = require('http')
+const express = require('express')
 const socketio = require('socket.io')
-const path =require('path')
 
-const app=express()
-const server =http.createServer(app)
-const io=socketio(server)
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
-
-const port =process.env.PORT || 3000
-const publicDirectoryPath=path.join(__dirname,'../public')
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-// let count =0
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
 
-//server(emit)=>client (recived) - countUpdated
-//client(emit)=>server(recived) - btn
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('message','A new user has joined!')
 
-io.on('connection',(socket)=>{
-console.log('New websocket connection')
-
-socket.emit('messageUpdate','Weclome to the server')
-
-socket.on('sendMessage',(message)=>{
-  io.emit('message',message)
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+    socket.on('disconnect',()=>{
+      io.emit('message','A user has left')
+    })
+    socket.on('sendLocation',(coords)=>{
+      io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    })
 })
 
-//emit- send data to client
-// socket.emit('countUpdated',count)
-
-// socket.on('btn',()=>{
-//      count++
-//    //socket.emit('countUpdated',count)
-//    io.emit('countUpdated',count)
-// })
- })
-
-server.listen(port,()=>{
-    console.log('server is up on the port ')
+server.listen(port, () => {
+    console.log(`Server is up on port${port}`)
 })
