0370c9c56b27964edabfa9ed2510ab98bdbda63d
sagnik3788 <116512372+sagnik3788@users.noreply.github.com>
2023-07-19 14:02:41 +0530

    updated

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,19 +17,29 @@ io.on('connection', (socket) => {
     console.log('New WebSocket connection')
 
     socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('message','A new user has joined!')
+    socket.broadcast.emit('message', 'A new user has joined!')
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
         io.emit('message', message)
+        callback()
     })
-    socket.on('disconnect',()=>{
-      io.emit('message','A user has left')
+
+    socket.on('sendLocation', (coords, callback) => {
+        io.emit('locationMessage', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
-    socket.on('sendLocation',(coords)=>{
-      io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!')
     })
 })
 
 server.listen(port, () => {
-    console.log(`Server is up on port${port}`)
+    console.log(`Server is up on port ${port}!`)
 })
